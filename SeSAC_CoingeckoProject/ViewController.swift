40d45088cf6bd6d2ec37b4308da3b60711f6638c
nhyeonjeong <hjnam5962@gmail.com>
2024-02-29 16:31:11 +0900

    Initial Commit

---
+++ SeSAC_CoingeckoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_CoingeckoProject
+//
+//  Created by 남현정 on 2024/02/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
